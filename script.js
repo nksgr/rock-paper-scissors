03fbb0a1ba2af0ae08bce9b6867e034f7962e89c
Niks <nikitha.sagar55@gmail.com>
2022-05-13 19:43:26 +0530

    Part 1 is completed and tested. What's in part1? You can use prompts to input game choices. Computer plays you. Round winner is decided. Each game has 5 rounds. You need to call game() manually from your console. The game is played entirely on console.

--- script.js
+++ script.js
@@ -1,77 +1,177 @@
 
+// part 1 - rock paper scissor - 
+
+//global variables
+
+moves = ['rock', 'paper', 'scissors']
+pScore = 0
+cScore = 0
+
+
+// list of functions: for part1 
+
+/*
+
+>>> game() - main game function - each game has 5 rounds <<< plays 1 game per func call
+
+
+supporting functions for game(): 
+
+1) pSelection - validates player's selection for the game
+
+2) cSelection - game move for computer chosen at random 
+
+3) roundWinner: decides who won the current round; calls roundPlay from here
+
+4) roundPlay - to battle out the choices; returns values: (0 - draw; 1: player win; -1: computer win) 
+
+5) gameWInneer: tally the score and decides who won the game
+
+
+*/
+
+
+
+
+
+// the main game function
+const game = ()=>{
+
+    pScore = 0;
+    cScore = 0;
+    
+
+    for (let i=1; i<6; i++){
+
+        console.log ("Round:", i)
+
+        let pSelect = pSelection()
+        let cSelect = cSelection() 
+
+        console.log("Computer CHOSE: ", cSelect)
+
+        console.log(roundWinner( pSelect, cSelect))
+
+        console.log(`Player: ${pScore} Computer: ${cScore} `)
+
+    }
+    
+    console.log(gameWinner(pScore, cScore))
+    
+
+
+}
+
+
+//supporting functions
+
+
+//pSelection - sanitizes user input... makes sure correct input is given by the user. 
+// it prompts until user enters correct input
+const pSelection = ()=>{
+
+    // for part 1 - we're gonna assume that user inputs the correct words 
+    let pSelect;
+    let msg = "Your turn: Rock, paper, scissors"
+    while(true){
+        pSelect = prompt(msg).toLowerCase()
+
+        if(moves.indexOf(pSelect) != -1)  break
+
+        else  msg = "Please enter the correct move: rock, paper, scissors"
+    }
+
+    return pSelect
+}
+
+// cSelection - computer makes a choice - based on random concept
 const cSelection = ()=>{
-    let moves = ['rock', 'paper', 'scissors']
-    let index = Math.floor( Math.random()*3)
+
+    let index = Math.floor(Math.random()*3)
 
     return moves[index]
 
 
 }
 
-const roundPlay = (player, comp)=>{
-    return 1;
+// here round winner is decided and is declared in the console
+
+    
+const roundWinner = (pSelect, cSelect)=> {
+    let roundScore = roundPlay(pSelect, cSelect) // calling roundplay function
+
+    switch(roundScore){
+        case -1: 
+            cScore++;
+            return (`${cSelect} beats ${pSelect}! You lost this round, booooo! `)
+            break 
+
+        case 1: 
+
+            pScore++;
+            return (`${pSelect} beats ${cSelect}! You won this round, yaaay! `)
+            break 
+
+        case 0: 
+
+            return (`You both drawed out.... Processor unhappy!`)
+    }
 }
 
-const game = ()=>{
-    let pScore = 0
-    let cScore = 0
-    for (let i=1; i<6; i++){
+// here score is decided. Return values: -1 --> if player loses ; 1 --> if player wins ; 0 --> if both draw out
 
-        let pSelect = "rock" // pSelection()
-        let cSelect = cSelection() 
+const roundPlay = (pSelect, cSelect)=>{
+    
+    switch(pSelect){
+        case 'rock' : 
 
-        console.log(cSelect)
-        let roundScore = roundPlay(pSelect, cSelect)
+            switch (cSelect){
+                case 'rock' : return 0;
+            
+                case 'paper': return -1;
 
-        switch(roundScore){
-            case -1: 
-                cScore++;
-                console.log(`${cSelect} beats ${pSelect}! You lost this round, booooo! `)
-                break 
+                case 'scissors': return 1;
+            }
+            
 
-            case 1: 
-
-                pScore++;
-                console.log(`${pSelect} beats ${cSelect}! You won this round, yaaay! `)
-                break 
-
-            case 0: 
-
-                console.log(`You both drawed out.... Processor unhappy!`)
-        }
+        case 'paper':
 
 
-        console.log(`Player: ${pScore} Computer: ${cScore} `)
+            switch (cSelect){
+                case 'rock' : return 1;
+            
+                case 'paper': return 0;
 
+                case 'scissors': return -1;
+            }
+            
 
+        case 'scissors':
+
+            switch (cSelect){
+                case 'rock' : return -1;
+            
+                case 'paper': return 1;
+
+                case 'scissors': return 0;
+            }
+            
     }
 
     
-    
-    
-
-
 }
+    
+// here, who won the game is decided. this function returns custom msgs 
 
-game()
-
-
-const pSelection = ()=>{
-    let pSelect
-    let regex = /[rock]/ig // write the regex here 
-    while(pSelect){
-
-        pSelect = prompt()
-
-        //use regex here to test for rock, paper, scissors, input.
-        //the loop keeps running until player inputs correct stuff.
-        // add a break statement if test() matches true
-
-        if(regex.test(pSelect)) break 
-        else {
-            console.log("Invalid choice... please enter again")
-        }
+const gameWinner = (pScore, cScore) =>{
+    if (pScore > cScore){
+        return "Yaaay! You won the game! You showed the computer who's the boss, for good!"
     }
-    //code
+    else if(cScore> pScore){
+        return "Yikes! Better luck next time. Computer beat you real bad, yo!"
+    }
 
+    else{
+        return "You both drawed out! No fun for me, processor was really betting on you, player!"
+    }
 }
